8a16da6e328decda74c5ccfd91eb49d654f5ed7c
Andrey Emelianov <621077@mail.ru>
2023-06-06 11:57:43 +0300

    Добавил компонент кнопки корзины

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -1,12 +1,13 @@
 import styles from './Header.module.css';
 import sushiImg from '../../../assets/sushi.jpg';
+import HeaderCartButton from '../HeaderCartButton/HeaderCartButton';
 
 const Header = () => {
 	return (
 		<>
 			<header className={styles.header}>
 				<h1>Японская кухня</h1>
-				<button>Корзина</button>
+				<HeaderCartButton />
 			</header>
 			<div className={styles['main-image']}>
 				<img src={sushiImg} alt="Блюда японской кухни" />
