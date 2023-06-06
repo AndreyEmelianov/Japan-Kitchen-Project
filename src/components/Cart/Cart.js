c26746742bcf87974d26b1b8221aade85709c852
Andrey Emelianov <621077@mail.ru>
2023-06-06 16:26:13 +0300

    добавил функционал добавления и удаление блюд в самой корзине на кнопки + и -

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -8,12 +8,16 @@ import styles from './Cart.module.css';
 const Cart = ({ onHideCartHandler }) => {
 	const cartContext = useContext(CartContext);
 
-	const totalAmount = `$${cartContext.totalAmount.toFixed(2)}`;
+	const totalAmount = `$${Math.abs(cartContext.totalAmount).toFixed(2)}`;
 	const hasItems = cartContext.items.length > 0;
 
-	const removeCartItemHandler = (id) => {};
+	const removeCartItemHandler = (id) => {
+		cartContext.removeItem(id);
+	};
 
-	const addCartItemHandler = (item) => {};
+	const addCartItemHandler = (item) => {
+		cartContext.addItem({ ...item, amount: 1 });
+	};
 
 	const cartItems = (
 		<ul className={styles['cart-items']}>
