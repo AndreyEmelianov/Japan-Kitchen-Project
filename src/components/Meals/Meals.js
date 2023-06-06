0021338e44e5055190dfe474509732d24b07ce90
Andrey Emelianov <621077@mail.ru>
2023-06-06 12:10:16 +0300

    Добавил компоненты для отображения текста о ресторане и для отображения списка блюд

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import PromoText from './PromoText/PromoText';
+import MealList from './MealList/MealList';
+
+const Meals = () => {
+	return (
+		<>
+			<PromoText />
+			<MealList />
+		</>
+	);
+};
+
+export default Meals;
