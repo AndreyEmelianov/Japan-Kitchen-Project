98fe376fa3c3dbdb804145a7dc83019a786985be
Andrey Emelianov <621077@mail.ru>
2023-06-06 16:34:20 +0300

    добавил анимацию для кнопки корзины при добавлении блюда, использовав useEffect

--- src/components/Layout/HeaderCartButton/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton/HeaderCartButton.js
@@ -1,18 +1,36 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import CartContext from '../../../store/cart-context';
 import CartIcon from '../../Cart/CartIcon/CartIcon';
 import styles from './HeaderCartButton.module.css';
 
 const HeaderCartButton = ({ onShowCartHandler }) => {
+	const [buttonIsAnimated, setButtonIsAnimated] = useState(false);
 	const cartContext = useContext(CartContext);
 
 	const cartItemsNumber = cartContext.items.reduce((currentValue, item) => {
 		return currentValue + item.amount;
 	}, 0);
 
+	const buttonClasses = `${styles.button} ${buttonIsAnimated ? styles.bump : ''}`;
+
+	useEffect(() => {
+		if (cartContext.items.length === 0) {
+			return;
+		}
+		setButtonIsAnimated(true);
+
+		const timer = setTimeout(() => {
+			setButtonIsAnimated(false);
+		}, 300);
+
+		return () => {
+			clearTimeout(timer);
+		};
+	}, [cartContext.items]);
+
 	return (
-		<button className={styles.button} onClick={onShowCartHandler}>
+		<button className={buttonClasses} onClick={onShowCartHandler}>
 			<span className={styles.icon}>
 				<CartIcon />
 			</span>
