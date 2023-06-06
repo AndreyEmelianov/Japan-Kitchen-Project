cfedf059d13c761a70fb02309efc78ab33a3471a
Andrey Emelianov <621077@mail.ru>
2023-06-06 16:01:59 +0300

    реализовал отображение добавленных блюд в корзине

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,21 +1,47 @@
+import { useContext } from 'react';
+
+import CartItem from './CartItem/CartItem';
+import CartContext from '../../store/cart-context';
 import Modal from '../UI/Modal/Modal';
 import styles from './Cart.module.css';
 
 const Cart = ({ onHideCartHandler }) => {
-	const cartItems = <ul className={styles['cart-items']}>{[]}</ul>;
+	const cartContext = useContext(CartContext);
+
+	const totalAmount = `$${cartContext.totalAmount.toFixed(2)}`;
+	const hasItems = cartContext.items.length > 0;
+
+	const removeCartItemHandler = (id) => {};
+
+	const addCartItemHandler = (item) => {};
+
+	const cartItems = (
+		<ul className={styles['cart-items']}>
+			{cartContext.items.map((item) => (
+				<CartItem
+					key={item.id}
+					name={item.name}
+					amount={item.amount}
+					price={item.price}
+					onAdd={addCartItemHandler.bind(null, item)}
+					onRemove={removeCartItemHandler.bind(null, item.id)}
+				/>
+			))}
+		</ul>
+	);
 
 	return (
 		<Modal onHideCartHandler={onHideCartHandler}>
 			{cartItems}
 			<div className={styles.total}>
 				<span>Итого</span>
-				<span>49.99</span>
+				<span>{totalAmount}</span>
 			</div>
 			<div className={styles.actions}>
 				<button className={styles['button--alt']} onClick={onHideCartHandler}>
 					Закрыть
 				</button>
-				<button className={styles.button}>Заказать</button>
+				{hasItems && <button className={styles.button}>Заказать</button>}
 			</div>
 		</Modal>
 	);
